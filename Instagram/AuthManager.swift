112ac176cadc454f773f474f8e49156ebe665bd7
Roy Park <roypark2638@gmail.com>
2021-03-06 12:28:45 -0800

    Add Manager Files

---
+++ Instagram/AuthManager.swift
@@ -0,0 +1,22 @@
+//
+//  AuthManager.swift
+//  Instagram
+//
+//  Created by Roy Park on 3/6/21.
+//
+
+import FirebaseAuth
+
+public class AuthManager {
+    static let shared = AuthManager()
+    
+    // MARK: - Public
+    
+    public func registerNewUser(username: String, email: String, password: String) {
+        
+    }
+    
+    public func loginUser(username: String?, email: String?, password: String) {
+        
+    }
+}
---
+++ Instagram/DatabaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  DatabaseManager.swift
+//  Instagram
+//
+//  Created by Roy Park on 3/6/21.
+//
+
+import FirebaseDatabase
+
+public class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    // MARK: - Public
+    
+}
