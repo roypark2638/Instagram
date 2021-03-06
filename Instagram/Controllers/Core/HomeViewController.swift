1def03b943dd499cd0438acb473af043d1231342
Roy Park <roypark2638@gmail.com>
2021-03-05 23:51:20 -0800

    Initial Commit

---
+++ Instagram/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Roy Park on 3/5/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
