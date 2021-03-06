fd48d5c34fe854753b33c39605dfae9413bdab63
Roy Park <roypark2638@gmail.com>
2021-03-06 12:27:46 -0800

    Check if user is login in or not to show which views in the beginning

--- Instagram/Controllers/Core/HomeViewController.swift
+++ Instagram/Controllers/Core/HomeViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // check auth status
+        handleNotAuthenticated()
     }
 
+    private func handleNotAuthenticated() {
+        if Auth.auth().currentUser == nil {
+            // show log in page
+            let loginVC = LoginViewController()
+            // set the loginVC fullScreen so that user can't swipe the page away
+            loginVC.modalPresentationStyle = .fullScreen
+            
+            present(loginVC, animated: false)
+        }
+        else {
+            
+        }
+        
+    }
 
 }
 
