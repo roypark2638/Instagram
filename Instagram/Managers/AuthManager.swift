a3e4a5c399b80894d65c1e44ed5257c38d0b3fb9
Roy Park <roypark2638@gmail.com>
2021-03-07 14:04:13 -0800

    Implemented registerNewUser logic. Check if username and email is available to create, then push account into database.

--- Instagram/Managers/AuthManager.swift
+++ Instagram/Managers/AuthManager.swift
@@ -12,8 +12,44 @@ public class AuthManager {
     
     // MARK: - Public
     
-    public func registerNewUser(username: String, email: String, password: String) {
-        
+    public func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        /*
+         - Check if username is available
+         - Check if email is available
+         */
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { (canCreate) in
+            if canCreate {
+                /*
+                - Create account in Firebase
+                - Insert account to database
+                */
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    guard result != nil, error == nil else {
+                        // Firebase auth coult not create an account
+                        completion(false)
+                        return
+                    }
+                    
+                    // Successfully created an account, insert into database
+                    DatabaseManager.shared.insertNewUser(with: email, username: username) { (inserted) in
+                        // success to insert into database
+                        if inserted {
+                            completion(true)
+                            return
+                        }
+                        // failed to insert into database
+                        else {
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            }
+            else {
+                // either username or email does not exist
+                completion(false)
+            }
+        }
     }
     
     // the completion return Bool value. The bool represents if the user successfully signin or not
