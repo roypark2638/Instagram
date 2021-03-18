7f43011473e53d92d851967727a61566d3ddb9e0
Roy Park <roypark2638@gmail.com>
2021-03-17 17:08:31 -0700

    Created PhotoCollctionViewCell for profile pictures

---
+++ Instagram/Views/Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Roy Park on 3/17/21.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PhotoCollectionViewCell"
+}
