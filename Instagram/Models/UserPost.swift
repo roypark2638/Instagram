ac7664d7b2b5fd9f6c5e89d42a7a5d4ba8db0de6
Roy Park <roypark2638@gmail.com>
2021-03-17 20:52:17 -0700

    Added more models for user info

--- Instagram/Models/UserPost.swift
+++ Instagram/Models/UserPost.swift
@@ -5,12 +5,59 @@
 //  Created by Roy Park on 3/10/21.
 //
 
-import Foundation
+import UIKit
 
 public enum UserPostType {
     case photo, video
 }
 
+
+/// representing user post information
 public struct UserPost {
+    let identifier: String
     let postType: UserPostType
+    let thumbnailImage: URL
+    let postURL: URL // either video url or full resolution photo
+    let caption: String? // user might provide the caption
+    let likeCount: [PostLike]
+    let comments: [PostComment]
+    let createdDate: Date
+    let taggedUser: [String]
+}
+
+enum Gender {
+    case male, female, other
+}
+
+struct User {
+    let username: String
+    let bio: String
+    let name: (first: String, last: String)
+    let birthDate: Date
+    let gender: Gender
+    let counts: UserCount
+}
+
+struct UserCount {
+    let followers: Int
+    let following: Int
+    let posts: Int
+}
+
+struct PostLike { // to see who has liked the post
+    let username: String
+    let postIdentifier: String
+}
+
+struct CommentLike { // to see who has liked the post
+    let username: String
+    let commentIdentifier: String
+}
+
+struct PostComment {
+    let identifier: String
+    let username: String
+    let text: String
+    let createdDate: Date
+    let likes: [CommentLike]
 }
