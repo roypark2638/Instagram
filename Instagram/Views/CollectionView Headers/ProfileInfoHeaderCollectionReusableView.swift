4b59151ea687ff9dd061ec12c8b8dd4078398792
Roy Park <roypark2638@gmail.com>
2021-03-17 17:08:50 -0700

    Created headersViews

---
+++ Instagram/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileInfoHeaderCollectionReusableView.swift
+//  Instagram
+//
+//  Created by Roy Park on 3/17/21.
+//
+
+import UIKit
+
+class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "ProfileInfoHeaderCollectionReusableView"
+}
---
+++ Instagram/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileTabsCollectionReusableView.swift
+//  Instagram
+//
+//  Created by Roy Park on 3/17/21.
+//
+
+import UIKit
+
+class ProfileTabsCollectionReusableView: UICollectionReusableView {
+    static let identifier = "ProfileTabsCollectionReusableView"
+}
