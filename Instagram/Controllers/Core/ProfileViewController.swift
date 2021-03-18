2206505adcc08029b361a3fdf1ffc8abc0bff27a
Roy Park <roypark2638@gmail.com>
2021-03-17 21:52:51 -0700

    configure the collectionView display headerView, TabView and collectionCell with the test image

--- Instagram/Controllers/Core/ProfileViewController.swift
+++ Instagram/Controllers/Core/ProfileViewController.swift
@@ -14,14 +14,15 @@ final class ProfileViewController: UIViewController {
     // we are going to instantiate it with a flow layout
     private var collectionView: UICollectionView?
     
+    private var userPosts = [UserPost]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
         configureNavigationBar()
         configureCollectionView()
-        
-        collectionView?.backgroundColor = .red
+                
         collectionView?.delegate = self
         collectionView?.dataSource = self
         
@@ -52,8 +53,11 @@ final class ProfileViewController: UIViewController {
         // somethings like scroll direction and item size
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 1, bottom: 0, right: 1)
+        let size = (view.width - 4) / 3
+        layout.itemSize = CGSize(width: size, height: size)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
         collectionView =  UICollectionView(frame: .zero, collectionViewLayout: layout)
     }
     
@@ -74,19 +78,95 @@ final class ProfileViewController: UIViewController {
 
 }
 extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 0
+        }
+//        return userPosts.count
         return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let model = userPosts[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier,
                                                       for: indexPath) as! PhotoCollectionViewCell
-        cell.backgroundColor = .blue
+//        cell.configure(with: model)
+        cell.configure(debug: "test")
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        
+        // when user tap the image, get the model and open post controller
+        let model = userPosts[indexPath.row]
+        let vc = PostViewController(model: model)
+        vc.title = "Posts"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader else { return UICollectionReusableView() } // if it's footer
+        
+        if indexPath.section == 1 {
+            // tabs header
+            let tabControlHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                         withReuseIdentifier: ProfileTabsCollectionReusableView.identifier,
+                                                                         for: indexPath) as! ProfileTabsCollectionReusableView
+            return tabControlHeader
+        }
+        
+        let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                     withReuseIdentifier: ProfileInfoHeaderCollectionReusableView.identifier,
+                                                                     for: indexPath) as! ProfileInfoHeaderCollectionReusableView
+        profileHeader.delegate = self
+        return profileHeader
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return CGSize(width: collectionView.width,
+                          height: collectionView.height/3)
+        }
+        return CGSize(width: collectionView.width,
+                      height: 65)
+    }
+    
+}
+
+// MARK: - ProfileInfoHeader Delegate
+
+extension ProfileViewController: ProfileInfoHeaderCollectionReusableViewDelegate {
+    func profileHeaderDidTapPostsButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        // scroll to the posts
+        collectionView?.scrollToItem(at: IndexPath(row: 0, section: 1), at: .top, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowersButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Followers"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowingButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Following"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapEditProfileButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = EditProfileViewController()
+        vc.title = "Edit Profile"
+        present(UINavigationController(rootViewController: vc), animated: true)
+    }
+    
     
 }
