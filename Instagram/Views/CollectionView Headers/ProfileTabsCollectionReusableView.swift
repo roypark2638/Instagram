46e45625e2e048ecbdf477531f30a9709ddaaf05
Roy Park <roypark2638@gmail.com>
2021-03-19 01:46:53 -0700

    Added Profile Reusable View

--- Instagram/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
+++ Instagram/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -7,15 +7,76 @@
 
 import UIKit
 
+protocol ProfileTabsCollectionReusableViewDelegate: AnyObject {
+    func didTapGridButtonTab()
+    func didTapTaggedButtonTab()
+}
 class ProfileTabsCollectionReusableView: UICollectionReusableView {
+    struct Constants {
+        static let padding: CGFloat = 4
+    }
+    
     static let identifier = "ProfileTabsCollectionReusableView"
+    
+    public weak var delegate: ProfileTabsCollectionReusableViewDelegate?
+    
+    private let gridButton: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.tintColor = .systemBlue
+        button.setBackgroundImage(UIImage(systemName: "square.grid.2x2"), for: .normal)
+        return button
+    }()
+    
+    private let taggedButton: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.tintColor = .gray
+        button.setBackgroundImage(UIImage(systemName: "tag"), for: .normal)
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .orange
+        backgroundColor = .secondarySystemBackground
+        addSubview(gridButton)
+        addSubview(taggedButton)
+        
+        gridButton.addTarget(self,
+                             action: #selector(didTapGridButton),
+                             for: .touchUpInside)
+        
+        taggedButton.addTarget(self,
+                             action: #selector(didTapTaggedButton),
+                             for: .touchUpInside)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size = height - (Constants.padding * 2)
+        let gridButtonX = ((width/2)-size)/2
+        gridButton.frame = CGRect(x: gridButtonX,
+                                  y: Constants.padding,
+                                  width: size,
+                                  height: size)
+        taggedButton.frame = CGRect(x: gridButtonX + (width/2),
+                                  y: Constants.padding,
+                                  width: size,
+                                  height: size)
+    }
+    
+    @objc private func didTapGridButton() {
+        
+    }
+    
+    @objc private func didTapTaggedButton() {
+        
+    }
+    
 }
+
+
